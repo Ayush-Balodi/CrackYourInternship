7106f461ad5b90b64c00837443d091c61b58f999
Ayush Balodi <ayushbalodidun@gmail.com>
2022-05-03 08:17:35 +0530

    find-the-duplicate-number

---
+++ find-the-duplicate-number.cpp
@@ -0,0 +1,21 @@
+/* 
+    https://github.com/Ayush-Balodi/CrackYourInternship.git
+
+    TC = O(n)
+    SC = O(1) as no extra space is consumed.
+*/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort( nums.begin() , nums.end() );
+        
+        int n = nums.size();
+        
+        for( int i=0 ; i<n-1 ; i++ ){
+            if( nums[i] == nums[i+1] ){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
