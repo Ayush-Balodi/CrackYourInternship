01fe5609a15a672df26be10684699f211cc1acd1
Ayush Balodi <ayushbalodidun@gmail.com>
2022-05-03 12:17:27 +0530

    find-the-duplicate-number using slow and fast pointer

--- find-the-duplicate-number.cpp
+++ find-the-duplicate-number.cpp
@@ -19,3 +19,25 @@ public:
         return -1;
     }
 };
+
+//@2nd method
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0] , fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while( slow != fast );
+        
+        fast = nums[0];
+        
+        while( slow != fast ){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return nums[slow];
+    }
+};
