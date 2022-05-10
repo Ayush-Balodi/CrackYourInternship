96e6094274868e7e6a73b2fcc8542c69bf0ebb8b
Ayush Balodi <ayushbalodidun@gmail.com>
2022-05-10 08:11:05 +0530

    two-sum with two pointer approach

--- two-sum.cpp
+++ two-sum.cpp
@@ -25,3 +25,40 @@ public:
         return v;
     }
 };
+
+/*
+    TC = O(n)
+    SC = O(p) + O(1) = O(p)
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        vector<pair<int,int>> p;
+        
+        for( int i=0 ; i<n ; i++ ){
+            p.push_back({nums[i],i});
+        }
+        vector<int> v;
+        
+        int i=0 , j=p.size()-1;
+        
+        sort( p.begin() , p.end() );
+        
+        while( i < j ){
+            if( (p[i].first + p[j].first) == target ){
+                v.push_back(p[i].second);
+                v.push_back(p[j].second);
+                return v;
+            }
+            else if ( (p[i].first + p[j].first) > target ){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return v;
+    }
+};
