24fbb9ee10aad788e5a8558f4c995d0004653085
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-06-11 23:04:35 +0530

    Subarray sum divsible by k using unordered map

--- subarray-sum-divisible-by-k.cpp
+++ subarray-sum-divisible-by-k.cpp
@@ -61,3 +61,28 @@ public:
         return c;
     }
 };
+
+/*
+    TC = O(n);
+    SC = O(n); as unordered_map can be of maximum size as n 
+*/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int temp , n=nums.size() , c=0;
+        unordered_map<int,int> m;
+        
+        m[0] = 1;
+        int sum=0;
+        for( int i=0 ; i<n ; i++ ){
+            
+            sum += nums[i];
+            if( m.count(sum-k) > 0 ){
+                 c += m[sum-k];
+            }
+            m[sum] += 1;
+        }
+        return c;
+    }
+};
