a10ad5f9bf9f3b089b990caa0b5806d8613333dc
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-06-13 09:06:04 +0530

    middle of the linkedlist in liear time

---
+++ middle-of-the-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+    https://github.com/Ayush-Balodi/CrackYourInternship.git
+    https://leetcode.com/problems/middle-of-the-linked-list
+
+    TC = O(n) n=>length of the linkedist
+    SC = O(1);
+*/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        int i=0 , j=0;
+        ListNode* temp = head;
+        while( temp != NULL ){
+            i++;
+            temp = temp->next;
+        }
+        
+        temp = head;
+        
+        if( i % 2 == 0 ){
+            i = (i / 2) + 1;
+            while( j < i-1 ){
+                temp = temp->next;
+                j++;
+            }
+        }
+        else{
+            i = (i / 2) + 1;
+            while( j < i-1 ){
+                temp = temp->next;
+                j++;
+            }
+        }
+        return temp;
+    }
+};
