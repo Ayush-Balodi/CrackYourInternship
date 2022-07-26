d207817c7238802eb64c16db5a1b7d9aa700411b
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-07-26 23:08:49 +0530

    first unique character in a string

---
+++ first-unique-character-in-a-string.cpp
@@ -0,0 +1,46 @@
+/*
+    https://github.com/Ayush-Balodi/CrackYourInternship.git
+    https://leetcode.com/problems/first-unique-character-in-a-string
+
+    TC = O(str.length())
+    SC = O(str.length())
+ 
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string str) {
+        
+        map<char, int> m;
+        map<char,int>::iterator itr;
+        
+        for( int i=0 ; i<str.length() ; i++){
+            char current = str[i];
+            if( m.find(current) == m.end() ) {
+                m.insert(make_pair(current,i));
+            }
+            else{
+                itr = m.find(current);
+                if( itr != m.end() ){
+                    itr->second = -1;
+                } 
+            }
+        }
+        
+        int min = INT_MAX;
+        for( itr = m.begin() ; itr!=m.end() ; ++itr){
+            
+            if( itr->second != -1 && itr->second < min ){
+                min = itr->second;
+            }
+        } 
+        
+        if( min == INT_MAX ){
+            return -1;
+        }
+        else{
+            return min;
+        }
+        return min;
+     }
+};
