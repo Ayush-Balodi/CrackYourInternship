b580912f9b388e4f38cdb1cb56ff539e64a537a6
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-08-08 22:57:35 +0530

    remove elements in java

--- remove-element.cpp
+++ remove-element.cpp
@@ -25,3 +25,25 @@ public:
         return n-c;       
     }
 };
+
+/*
+    TC = O(n)
+    SC = O(1)
+*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int n = nums.length;
+        
+        int i=0 ;
+        for( int j=0 ; j<n ; j++ ){
+            
+            if( nums[j] != val ){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
