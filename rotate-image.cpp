74d1f0e6639ce3d77961b7d162db647138aeb351
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-08-15 08:42:03 +0530

    rotate image using Java

--- rotate-image.cpp
+++ rotate-image.cpp
@@ -2,7 +2,7 @@
     https://github.com/Ayush-Balodi/CrackYourInternship.git
     https://leetcode.com/problems/rotate-image/
     
-    TC = O(n^2) + O(n^2)
+    TC = O(n^2) + O(n^2) => O(n^2)
     SC = O(1)
 */
 
@@ -23,3 +23,30 @@ public:
         }
     }
 };
+
+/*
+    Java Solution
+    TC = O(n^2) + O(n^2) => O(n^2)
+    SC = O(1)
+*/
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for( int i=0 ; i<matrix.length ; i++ ){
+            for( int j=0 ; j<i ; j++ ){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for( int i=0 ; i<matrix.length ; i++ ){
+            for( int j=0 ; j<matrix[0].length/2 ; j++ ){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
