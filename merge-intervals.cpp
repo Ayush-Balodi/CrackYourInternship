03726dc0d041646daa2cb67fbfb606d40b7cd847
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-10-06 23:08:23 +0530

    merge intervals using c++

---
+++ merge-intervals.cpp
@@ -0,0 +1,33 @@
+/*
+    https://github.com/Ayush-Balodi/CrackYourInternship.git
+    https://leetcode.com/problems/contains-duplicate-ii/
+
+    TC = O(nlog(n)) + O(n) = O(nlog(n))
+    SC = O(n) 
+    Auxiliary space = O(n)
+*/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+        
+        vector<vector<int>> mergeInterval;
+        if( interval.size() == 0 ){
+            return mergeInterval;
+        }
+        sort(interval.begin() , interval.end());
+        vector<int> tempInterval = interval[0];
+        
+        for(auto it : interval){  
+            if( it[0] <= tempInterval[1] ){
+                tempInterval[1] = max(it[1] ,tempInterval[1]);
+            }
+            else{
+                mergeInterval.push_back(tempInterval);
+                tempInterval = it;
+            }
+        }
+        mergeInterval.push_back(tempInterval);
+        return mergeInterval;
+    }
+};
