d9c6a79c3676bec4683d32cd7b2e875cea0b8572
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2022-11-07 23:34:38 +0530

    climbing stairs

--- climbing-stairs.cpp
+++ climbing-stairs.cpp
@@ -3,9 +3,28 @@
     https://leetcode.com/problems/climbing-stairs
     
     TC = O(n)
-    SC = O(1)
+    Auxiliary space = O(n) 
 */
-
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if( n<=2 ){
+            return n;
+        }
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for( int i=3; i<=n ; i++ ){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+/*
+    TC = O(n)
+    Auxiliary space = O(1) // as no extra space is taken 
+*/
 class Solution {
 public:
     int climbStairs(int n) {
