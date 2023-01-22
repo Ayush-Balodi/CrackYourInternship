6eb72327b579452e67431c3c448fd61927f94ede
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-01-22 21:00:21 +0530

    Time: 11 ms (93.38%), Space: 10.2 MB (66.96%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -7,21 +7,21 @@ public:
         priority_queue<pair<int,int>> pq;
         
         for( int i=0 ; i<n ; i++ ){
-            pq.push({score[i],i});
+            pq.push(make_pair(score[i],i));    
         }
         
         for( int i=0 ; i<n ; i++ ){
-            if( i == 0 ){
+            if( i==0 ){
                 str[pq.top().second] += "Gold Medal";
             }
-            else if( i == 1 ){
+            else if( i==1 ){
                 str[pq.top().second] += "Silver Medal";
             }
-            else if( i == 2 ){
+            else if( i==2 ){
                 str[pq.top().second] += "Bronze Medal";
             }
             else{
-                str[pq.top().second] += to_string(i+1);
+                str[pq.top().second] = to_string(i+1);
             }
             pq.pop();
         }
