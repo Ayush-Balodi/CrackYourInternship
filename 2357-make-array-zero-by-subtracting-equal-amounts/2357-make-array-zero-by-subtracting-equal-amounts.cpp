137c27afaa3cae11403a0f4e30a97fd0fcc9b49b
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-01-22 22:40:21 +0530

    Time: 3 ms (68.14%), Space: 8.4 MB (60.93%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        unordered_set<int> un;
-        for( auto &x : nums ){
-            if( x!=0 ){ un.insert(x); }
+        
+        int n=nums.size();
+        priority_queue<int,vector<int>,greater<int>> pq;
+        int mx=INT_MIN;
+        
+        for( int i=0 ; i<n ; i++ ){
+            pq.push(nums[i]);
+            mx = max( mx,nums[i] );
+        } 
+        
+        int temp=0, count=0, lastElement=0;
+        while( !pq.empty() && mx ){
+            
+            if( pq.top()==0 || pq.top()==lastElement ){
+                pq.pop();
+                continue;
+            }
+            
+            int topElement = pq.top();
+            int value = topElement-temp;
+            mx -= value;
+            temp += value;
+            
+            count++;
+            lastElement = topElement;
         }
-        return un.size();
+        return count;
     }
 };
