6169d4f3cff87b16ec5290e93b3fae3c0ea8b2f0
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-13 22:31:59 +0530

    Time: 13 ms (75.72%), Space: 11.1 MB (18.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
+        
+        int  n = nums.size();
+        vector<pair<int,int>> v;
+        for( int i=0 ; i<n ; i++ ){
+            v.push_back(make_pair(nums[i],i));
+        }
+        
+        sort(v.begin() , v.end());
+        
+        int low=0, high=n-1;
         vector<int> ans;
-        for( int i=0 ; i<n-1 ; i++ ){
-            for( int j=i+1 ; j<n ; j++ ){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        
+        while( low <= high ){
+            if( (v[low].first + v[high].first) == target){
+                ans.push_back(v[low].second);
+                ans.push_back(v[high].second);
+                break;
+            }
+            else if( (v[low].first + v[high].first) < target ){
+                low++;
+            }
+            else{
+                high--;
             }
         }
         return ans;
