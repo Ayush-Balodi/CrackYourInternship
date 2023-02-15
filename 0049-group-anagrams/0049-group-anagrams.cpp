9803a712c62451fde65dff92b2ecaea78fe01bf3
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-15 07:14:38 +0530

    Time: 32 ms (89.96%), Space: 18 MB (97.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-     
-        int i=0, n=strs.size();
+        
+        int n=strs.size(), i=0;
         vector<pair<string,int>> v;
+        
         for( auto s:strs ){
-            sort(s.begin() , s.end());
+            sort(s.begin(), s.end());
             v.push_back(make_pair(s,i++));
         }
         
@@ -15,13 +16,13 @@ public:
         vector<string> temp;
         
         for( i=0 ; i<n-1 ; i++ ){
-            if(v[i].first == v[i+1].first){
+            if( v[i].first == v[i+1].first ){
                 temp.push_back(strs[v[i].second]);
             }
-            if(v[i].first != v[i+1].first){
+            if( v[i].first != v[i+1].first ){
                 temp.push_back(strs[v[i].second]);
                 ans.push_back(temp);
-                temp.clear();                
+                temp.clear();
             }
         }
         temp.push_back(strs[v[i].second]);
