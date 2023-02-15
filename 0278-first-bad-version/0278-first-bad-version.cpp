a8c90496c37ffda689cda916e83e3dd57e9e3070
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-15 09:58:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.00%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,13 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int ans;
-        for( int i=n ; i>=0 ; i-- ){
-            if(isBadVersion(i) == false){
-                ans = i+1;
-                break;
+        int low=0, high=n-1;
+        while( low<=high ){
+            int mid = low+(high-low)/2;
+            if( !isBadVersion(mid) ){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-        return ans;
+        return low;
     }
 };
