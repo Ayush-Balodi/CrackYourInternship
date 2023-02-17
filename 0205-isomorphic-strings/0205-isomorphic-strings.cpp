e8252e02b110fa9a1f8d9346fe47ea89655d2a66
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-17 17:42:11 +0530

    Time: 9 ms (53.86%), Space: 7.1 MB (18.23%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n=s.length();
+        unordered_map<char,char> mp1, mp2;
+        for( int i=0 ; i<n ; i++ ){
+            if(mp1[s[i]] && mp1[s[i]]!=t[i]){ return false; }
+            if(mp2[t[i]] && mp2[t[i]]!=s[i]){ return false; }
+            mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
+        }
+        return true;
+    }
+};
+
+// I am just saving the character matching in both string
+// If found not same at any time just returning false
+// Else if not found making the new dictionary about it.
