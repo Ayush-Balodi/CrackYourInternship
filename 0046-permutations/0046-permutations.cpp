12155c3c4cbd9b64760fc4af196a1f6c89c6be66
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-21 06:48:12 +0530

    Time: 10 ms (10.61%), Space: 8.3 MB (14.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,3 +1,11 @@
+/*
+1> We are traversing the nums by taking a data structure and having the frequency map to store which element we have currently in our data structure.
+
+2> We are calculating this in permute function where we are checking whether the frequency map of number is unmarked or not, if it is unmarked then move forward to add it in the data structure and make its count '1' so as to mark it down and recursively call back to the function with same parameters so as to have another element left.
+
+3> When returning from the function we are just removing the currently added element to the data structure and also removing them from frequency map as '0' marked.
+
+*/
 class Solution {
 private:
     void permute( vector<vector<int>> &ans, vector<int> &nums, vector<int> ds, int freq[], int n ){
