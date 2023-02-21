465fd790fcd41346f2b2d897615943a4c998c54d
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-21 23:02:51 +0530

    Time: 11 ms (83.80%), Space: 12.8 MB (44.68%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    typedef pair<string,int> P;
-    
-    struct thetha{
-        bool operator()(P &a, P &b ){
-            return (a.second>b.second) || (a.second==b.second && a.first<b.first);
-        }    
-    };
-    
     vector<string> topKFrequent(vector<string>& words, int k) {
-        
-        priority_queue<P , vector<P> , thetha> pq;
         
         unordered_map<string,int> mp;
         for( auto it:words ){
             mp[it]++;
         }
         
+        vector<pair<string,int>> v;
         for( auto it:mp ){
-            pq.push({it.first, it.second});
-            if(pq.size() > k)
-                pq.pop();
+            v.push_back({it.first,it.second});
         }
         
-        int i = k-1;
-        vector<string> result(k);
-        while(!pq.empty()) {
-            result[i] = pq.top().first;
-            pq.pop();
-            i--;
+        sort( v.begin() , v.end() , [&]( auto &a, auto &b ){
+            if( a.second == b.second ){
+                return a.first<b.first;
+            }
+            return a.second>b.second;
+        });
+        
+        vector<string> answer;
+        for( int i=0 ; i<k ; i++ ){
+            answer.push_back(v[i].first);
         }
-        return result;
+        return answer;
     }
 };
