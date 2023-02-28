a534339b61ad7f907d938f34cf916638ce8ce9f2
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-02-28 11:09:07 +0530

    Time: 9 ms (90.09%), Space: 10.4 MB (15.57%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -4,6 +4,9 @@ public:
         
         int maxval = INT_MIN,cnt=0;
         for( auto x:sentences ){
+            // maxval = max(maxval,(int)count(x.begin(),x.end(),' '));
+            // return maxval+1;
+            // (int) because of the returning of unsigned int by count function
             cnt = 1+count(x.begin(),x.end(),' ');
             maxval = max(cnt,maxval);
         }
