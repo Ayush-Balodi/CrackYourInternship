fd61326185135fd0f8bedd8081c2a8ad4da0e56d
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-04 11:59:32 +0530

    Time: 39 ms (67.61%), Space: 18.3 MB (96.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        int n=strs.size(), i=0;
         vector<pair<string,int>> v;
-        
-        for( auto s:strs ){
-            sort(s.begin(), s.end());
-            v.push_back(make_pair(s,i++));
+        int i=0;
+        for( auto x:strs ){
+            sort(x.begin(),x.end());
+            v.push_back({x,i++});
         }
         
-        sort(v.begin() , v.end());
+        sort(v.begin(),v.end());
         
+        int n=v.size();
         vector<vector<string>> ans;
         vector<string> temp;
-        
-        for( i=0 ; i<n-1 ; i++ ){
-            if( v[i].first == v[i+1].first ){
+        for( int i=0 ; i<n-1 ; i++ ){
+            if(v[i].first == v[i+1].first){
                 temp.push_back(strs[v[i].second]);
             }
-            if( v[i].first != v[i+1].first ){
+            if(v[i].first != v[i+1].first){
                 temp.push_back(strs[v[i].second]);
                 ans.push_back(temp);
                 temp.clear();
-            }
+            }   
         }
-        temp.push_back(strs[v[i].second]);
+        temp.push_back(strs[v[n-1].second]);
         ans.push_back(temp);
-        
         return ans;
     }
 };
