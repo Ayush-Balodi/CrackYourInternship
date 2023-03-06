79f8ddb15aaf775f652c17615cd690431432ddb7
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-06 16:35:17 +0530

    Time: 4 ms (71.44%), Space: 6.8 MB (22.96%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         
-        int n=secret.size(), bull=0;
-        unordered_map<int,int> m1;
-        unordered_map<int,int> m2;
+        int bulls=0, cows=0;
+        int n=secret.length(), m=guess.length();
+        unordered_map<char,int> mp1;
+        unordered_map<char,int> mp2;
         
-        for( int i=0 ; i<n; i++ ){
-            if(secret[i] == guess[i]){
-                bull++;
+        for( int i=0 ; i<n ; i++ ){
+            if( secret[i] == guess[i] ){
+                bulls++;
             }
             else{
-                m1[secret[i]]++;
-                m2[guess[i]]++;
-            }
-        }        
-        int cow=0;
-        for( auto val:m2 ){
-            if(m1.find(val.first) != m1.end()){
-                cow += min(val.second,m1[val.first]);
+                mp1[secret[i]]++;
+                mp2[guess[i]]++;
             }
         }
-        string ans = to_string(bull) + "A" + to_string(cow)+ "B";
-        return ans;
+        for( auto val:mp2 ){
+            if( mp1.find(val.first) != mp1.end() ){
+                cows += min(val.second,mp1[val.first]);
+            }
+        }
+        return to_string(bulls)+'A'+to_string(cows)+'B';
     }
 };
+/*
+    [1123]
+      |
+    [01*1] *->1
+*/
