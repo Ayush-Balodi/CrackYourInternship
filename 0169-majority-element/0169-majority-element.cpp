4f688e6510d077753165fdfab84496fa319db26f
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-06 23:49:30 +0530

    Time: 27 ms (28.40%), Space: 19.5 MB (80.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         
         int n=nums.size()/2;
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         for( auto x:nums ){
             mp[x]++;
         }
