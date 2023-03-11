5d4c9223db552e69b5815bd18c1f8c9bea0e12ca
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-11 22:31:46 +0530

    Time: 58 ms (23.41%), Space: 24.5 MB (5.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string str1, string str2) {
-        
-        int n=str1.size() , m=str2.size();
-        int dp[n+1][m+1];
-        memset( dp , 0 , sizeof(dp) );
-        
-        for( int i=1 ; i<=n ; i++ ){
-            for( int j=1 ; j<=m ; j++ ){
-                if( str1[i-1] == str2[j-1] ){
-                    dp[i][j] = dp[i-1][j-1] + 1;
+    // int solve( string str1, string str2, int i, int j, vector<vector<int>> &dp ){
+    //     if( i==str1.length() ){
+    //         return 0;
+    //     }
+    //     if( j==str2.length() ){
+    //         return 0;
+    //     }
+    //     if( dp[i][j] != -1 ){
+    //         return dp[i][j];
+    //     } 
+    //     int ans=0;
+    //     if( str1[i] == str2[j] ){
+    //         ans = 1+solve(str1, str2, i+1, j+1, dp);
+    //     }
+    //     else{
+    //         ans = max(solve(str1, str2, i+1, j, dp), solve(str1, str2, i, j+1, dp));
+    //     }
+    //     return dp[i][j] = ans;
+    // }
+    int solveTab( string str1, string str2, int n, int m ){
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for( int i=n-1 ; i>=0 ; i-- ){
+            for( int j=m-1 ; j>=0 ; j-- ){
+                int ans=0;
+                if( str1[i] == str2[j] ){
+                    ans = 1+dp[i+1][j+1];
                 }
                 else{
-                    dp[i][j] = max( dp[i][j-1] , dp[i-1][j] );
+                    ans = max(dp[i+1][j], dp[i][j+1]);
                 }
+                dp[i][j] = ans;
             }
         }
-        return dp[n][m];
+        return dp[0][0];
+    }
+    int longestCommonSubsequence(string str1, string str2) {
+        int n=str1.length(), m=str2.length();
+        return solveTab(str1, str2, n, m);
+        // vector<vector<int>> dp(str1.length(), vector<int>(str2.length(),-1));
+        // return solve(str1, str2, 0, 0, dp);
     }
 };
