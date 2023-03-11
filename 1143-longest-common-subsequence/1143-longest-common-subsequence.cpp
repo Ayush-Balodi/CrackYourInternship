45f47cd4bd3405403c288befde8ad5c7eab788c2
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-11 22:49:56 +0530

    Time: 46 ms (47.47%), Space: 6.6 MB (91.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*DP TOP DOWN OR MEMOIZATION*/
     // int solve( string str1, string str2, int i, int j, vector<vector<int>> &dp ){
     //     if( i==str1.length() ){
     //         return 0;
@@ -19,21 +20,42 @@ public:
     //     }
     //     return dp[i][j] = ans;
     // }
+    /*DP BOTTOM UP*/
+    // int solveTab( string str1, string str2, int n, int m ){
+    //     vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+    //     for( int i=n-1 ; i>=0 ; i-- ){
+    //         for( int j=m-1 ; j>=0 ; j-- ){
+    //             int ans=0;
+    //             if( str1[i] == str2[j] ){
+    //                 ans = 1+dp[i+1][j+1];
+    //             }
+    //             else{
+    //                 ans = max(dp[i+1][j], dp[i][j+1]);
+    //             }
+    //             dp[i][j] = ans;
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    /*DP BOTTOM UP REDUCED SPACE COMPLEXICITY*/
     int solveTab( string str1, string str2, int n, int m ){
-        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        vector<int> curr(m+1, 0);
+        vector<int> next(m+1, 0);
         for( int i=n-1 ; i>=0 ; i-- ){
             for( int j=m-1 ; j>=0 ; j-- ){
                 int ans=0;
                 if( str1[i] == str2[j] ){
-                    ans = 1+dp[i+1][j+1];
+                    ans = 1+next[j+1];
                 }
                 else{
-                    ans = max(dp[i+1][j], dp[i][j+1]);
+                    ans = max(next[j], curr[j+1]);
                 }
-                dp[i][j] = ans;
+                curr[j] = ans;
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     int longestCommonSubsequence(string str1, string str2) {
         int n=str1.length(), m=str2.length();
