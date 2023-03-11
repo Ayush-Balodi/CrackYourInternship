14b41ddef58f796a52f0dd9e888a544eb17707e7
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-12 01:29:17 +0530

    Time: 37 ms (23.51%), Space: 20.1 MB (24.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -8,13 +8,14 @@ private:
     //     return (s[i]==s[j]) ? helper( s, i+1, j-1 ) : 0;
     // }
     
-    int helper( vector<vector<int>> &mem, string s, int i, int j ){
-        if( i >= j ){
-            return 1;
-        }
-        if( mem[i][j] >=0 ){ return mem[i][j]; }
-        return mem[i][j] = (s[i]==s[j]) ? helper( mem, s, i+1, j-1 ) : 0;
-    }
+    /*2.> Memoized approach*/
+    // int helper( vector<vector<int>> &mem, string s, int i, int j ){
+    //     if( i >= j ){
+    //         return 1;
+    //     }
+    //     if( mem[i][j] >=0 ){ return mem[i][j]; }
+    //     return mem[i][j] = (s[i]==s[j]) ? helper( mem, s, i+1, j-1 ) : 0;
+    // }
 public:
     int countSubstrings(string s) {
         /*1.> Recursive*/
@@ -26,13 +27,41 @@ public:
         // }
         // return count;
         
-        vector<vector<int>> mem(s.length() , vector<int>(s.length(), -1));
-        int count=0;
-        for( int i=0 ; i<s.size() ; i++ ){
-            for( int j=i ; j<s.size() ; j++ ){
-                count += helper(mem,s,i,j);
+        /*2.> Memoized approach*/
+        // vector<vector<int>> mem(s.length() , vector<int>(s.length(), -1));
+        // int count=0;
+        // for( int i=0 ; i<s.size() ; i++ ){
+        //     for( int j=i ; j<s.size() ; j++ ){
+        //         count += helper(mem,s,i,j);
+        //     }
+        // }
+        // return count;
+        
+        /*3.> DP*/
+        int n=s.length(), count=0;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for( int i=n-1 ; i>=0 ; i-- ){
+            for( int j=i ; j<n ; j++ ){
+                if( i == j ){
+                    dp[i][j] += 1;
+                }
+                else if( i+1 == j ){
+                    dp[i][j] = (s[i]==s[j]) ? 1 : 0;
+                }
+                else{
+                    dp[i][j] = (s[i]==s[j]) ? dp[i+1][j-1] : 0;
+                }
+                count += dp[i][j];
             }
         }
         return count;
     }
 };
+/*
+i=0 j=2<3
+     a  b  c
+     0  1  2
+a 0  1  0  0
+b 1  0  1  0
+c 2  0  0  1
+*/
