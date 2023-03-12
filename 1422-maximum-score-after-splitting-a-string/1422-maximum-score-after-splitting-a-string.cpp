685ab8f7d66639861e9e157397a5f67520269909
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-12 20:08:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (99.04%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n=s.length();
-        string t1, t2;
-        int sum=0;
+        int one=0, zero=0;
+        for( auto x:s ){
+            if( x == '1' )
+                one++;
+        }
+        int n=s.length(), sum=0;
         for( int i=0 ; i<n-1 ; i++ ){
-            t1 = s.substr(0,i+1);
-            t2 = s.substr(i+1);
-            cout << t1 << " " << t2<<endl;
-            int num = count(t1.begin(),t1.end(),'0') + count(t2.begin(),t2.end(),'1');
-            sum = max(sum,num);
-            t1.clear(); t2.clear();
+            if(s[i] == '0'){
+                zero++;
+            }
+            else{
+                one--;
+            }
+            sum = max(sum,zero+one);
         }
         return sum;
     }
