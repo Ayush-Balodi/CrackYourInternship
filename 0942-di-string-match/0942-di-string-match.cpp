28c21e2c4c850854d2b39ca2588353eb9f0f2529
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-13 18:46:14 +0530

    Time: 3 ms (92.23%), Space: 8.5 MB (93.54%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cpp
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        int n=s.size();
-        deque<int> pq;
-        for( int i=0;i<=n;i++){
-            pq.push_back(i);
+        int n=s.length(), left=0, right=n;
+        vector<int> answer(n+1, 0);
+        for( int i=0; i<n ; i++ ){
+            answer[i] = s[i] == 'I' ? left++ : right--;  
         }
-        vector<int> v;
-        for( int i=0 ; i<n ; i++ ){
-            if(s[i] == 'I'){
-                v.push_back(pq.front());
-                pq.pop_front();
-            }
-            if( s[i] == 'D' ){
-                v.push_back(pq.back());
-                pq.pop_back();
-            }
-        }
-        v.push_back(pq.back());
-        return v;
+        answer[n] = left;
+        return answer;
     }
 };
