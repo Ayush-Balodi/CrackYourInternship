87a8724736b7b61e477d95e446a07c8ddc1e4f58
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-17 09:12:14 +0530

    Time: 12 ms (93.76%), Space: 26.6 MB (19.41%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,15 +11,7 @@
  */
 class Solution {
 public:
-    int findPos( vector<int> in, int val, int n ){
-        for( int i=0 ; i<n ; i++ ){
-            if( in[i] == val ){
-                return i;
-            } 
-        }
-        return -1;
-    }
-    TreeNode* helper( vector<int> &in, vector<int> &pre , int &index, int inIndex , int endIndex , int n ){
+    TreeNode* helper( vector<int> &in, vector<int> &pre , int &index, int inIndex , int endIndex , int n , map<int,int> &mp){
         // base case
         if( index >= n || inIndex > endIndex ){
             return NULL;
@@ -27,19 +19,23 @@ public:
         
         int element = pre[index];
         TreeNode* root = new TreeNode(element);
-        int position = findPos(in, element, n);
+        int position = mp[element];
         index++;
         
         //recursive calls
-        root->left = helper(in, pre, index, inIndex, position-1, n );
-        root->right = helper(in, pre, index, position+1, endIndex, n);
+        root->left = helper(in, pre, index, inIndex, position-1, n , mp );
+        root->right = helper(in, pre, index, position+1, endIndex, n, mp);
         return root;     
     }
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int preIndex = 0;
         int n=inorder.size();
-        TreeNode* root = helper(inorder, preorder, preIndex, 0, n-1, n );
+        map<int,int> mp;
+        for( int i=0 ; i<n ; i++ ){
+            mp[inorder[i]] = i;
+        }
+        TreeNode* root = helper(inorder, preorder, preIndex, 0, n-1, n ,mp);
         return root;
     }
 };
