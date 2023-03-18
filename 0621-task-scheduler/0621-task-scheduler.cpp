775d2287470fd55cdda3afc2f0377d3a192bc455
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-18 20:25:59 +0530

    Time: 101 ms (44.25%), Space: 37.9 MB (36.79%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,39 +1,32 @@
-/*
-    1.> First I've made a frequency map
-    2.> Then build a maxheap based on the frequency
-    3.> I've make the queue which is consisting of time period for which cpu will remain ideal if we use that character again
-    4.> We iterate and do this task until the q is not empty or pq is not empty
-*/
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
-        priority_queue<int> pq;
-        map<char, int> mp;
+        unordered_map<char,int> mp;
         for( auto x:tasks ){
             mp[x]++;
         }
         
+        priority_queue<int> pq;
         for( auto x:mp ){
             pq.push(x.second);
         }
-        
-        queue<pair<int,int>> q; 
         int time=0;
+        queue<pair<int,int>> q;
+        
         while( !pq.empty() || !q.empty() ){
-            time += 1;
+            time+=1;
             if( !pq.empty() ){
                 int temp = pq.top();
-                pq.pop();
                 temp--;
-                if( temp != 0 ){
-                    q.push({time+n,temp});
+                pq.pop();
+                if( temp!=0 ){
+                    q.push( {time+n, temp} );
                 }
-            }
-            if( !q.empty() and q.front().first==time ){
+            } 
+            if( !q.empty() and q.front().first == time ){
                 auto temp2 = q.front();
-                pq.push(temp2.second);
                 q.pop();
+                pq.push({temp2.second});
             }
         }
         return time;
