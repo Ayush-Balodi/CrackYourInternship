c1d3d1162e104869e9a88c05f5103fcb223884bb
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-19 20:21:20 +0530

    Time: 19 ms (66.67%), Space: 29 MB (33.33%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -5,21 +5,25 @@ public:
         vector<vector<int>> valid={{-2,-1},{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2}};
         int n=grid.size();
         int count=1, l=0, r=0;
-        while( count < n*n ){
+        int num = n*n-1;
+        
+        while( num-- ){
+            
             bool flag = false;
             for( int i=0 ; i<8 ; i++ ){
-                int t1=l, t2=r;
-                l = l+ valid[i][0];
-                r = r+ valid[i][1];
-                if( l>=0 && l<n && r>=0 && r<n && grid[l][r] == count ){
+        
+                int newL = l+ valid[i][0];
+                int newR = r+ valid[i][1];
+                if( newL>=0 && newL<n && newR>=0 && newR<n && grid[newL][newR] == count ){
+                    
+                    l=newL, r=newR;
                     flag = true;
                     break;
                 }
-                l=t1,r=t2;
             }
-            if( !flag )
-                return false;
             count++;
+            if( flag == false )
+                return false;
         }
         return true;
     }
