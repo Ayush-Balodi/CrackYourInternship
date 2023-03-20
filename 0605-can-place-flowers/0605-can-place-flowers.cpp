edcdb64733e0fdbc1ac211c5ad1555fb56017370
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-20 15:38:05 +0530

    Time: 18 ms (67.22%), Space: 20.3 MB (76.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& nums, int key) {
-        
-        if( key == 0 ){
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+       int left,right;
+        if(flowerbed.size()==1 && n==1 && flowerbed[0]==0)
             return true;
-        }
-        int count=0;
-        int n=nums.size();
-        
-        if( n==1 and nums[0]==0 ){
-            return true;
-        }
-        for( int i=0 ; i<n ; i++ ){
-
-            if(i==0 and nums[i]==0 and !nums[i+1] ){
-                nums[i] = 1;
-                count++;
-            }
-            if( i==n-1 and nums[i]==0 and !nums[i-1] ){
-                nums[i] = 1;
-                count++;
-            } 
-            if( (i!=0 and i!=n-1 ) and nums[i]==0 and !nums[i-1] and !nums[i+1] )
+        int count =0;
+        for(int i=0;i<flowerbed.size();i++)
+        {
+            if(flowerbed[i]==1)
             {
-                nums[i] = 1;
-                count++;
+                continue; 
             }
-            if( count == key ){
-                return true;
+            else
+            {
+                if(i-1>=0 && flowerbed[i-1]!=1 && i+1<flowerbed.size() && flowerbed[i+1]!=1)
+                {
+                    count++;
+                    flowerbed[i]=1;
+                }
+                else if (i+1>=flowerbed.size()&& i-1>=0 && flowerbed[i-1]!=1)
+                {
+                    count++;
+                    flowerbed[i]=1;
+                }
+                else if(i-1<0 && i+1<flowerbed.size() && flowerbed[i+1]!=1)
+                {
+                    count++;
+                    flowerbed[i]=1;
+                }
             }
         }
+        
+        if(count>=n)
+             return true;
+        
         return false;
     }
 };
