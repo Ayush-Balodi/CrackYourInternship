9e4fd03878ef22fdf0f6386a048c391ae978cdcf
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-29 08:08:50 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (66.55%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        int l=0, h=n-1;
-        int alice=0, bob=0;
-        bool flag=false;
-        while( l<=h ){
-            if( flag==false ){
-                if(piles[l] > piles[h]){
-                    alice+=piles[l]; 
-                    l++;
-                }else{
-                    alice+=piles[h]; 
-                    h--;
-                }
-            }
-            else{
-                if(piles[l] > piles[h]){
-                    bob+=piles[l]; 
-                    l++;
-                }else{
-                    bob+=piles[h]; 
-                    h--;
-                }
-            }
-        }
-        
-        if( alice > bob )
-            return true;
-        return false;
+        return true;
     }
 };
