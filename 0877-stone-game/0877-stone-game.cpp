d7d5e992088e94b9569aa801793bb226295a6059
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-03-29 09:15:35 +0530

    Time: 19 ms (33.35%), Space: 8.6 MB (38.09%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
+    int dp[501][501];
+    int helper(int i, int j, vector<int> &p ){
+        if( i>j )
+            return 0;
+        if( dp[i][j] != -1 )
+            return dp[i][j];
+        return dp[i][j]=max(p[i]-helper(i+1, j, p), helper(i, j-1, p));
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp, -1, sizeof(dp));
+        return helper(0,piles.size()-1, piles)>0;
     }
 };
