54fcf8a9d0726d1ad7c9babe503e2ceb0a1eff1d
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-02 12:58:22 +0530

    Time: 3 ms (77.85%), Space: 7.2 MB (16.64%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
-
+    
+    string code="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%0123456789";
+    //unordered_map<string,string> mp1;
+    unordered_map<string,string> mp2;
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        
+        string encoded=convert();
+        
+        while( mp2.find(encoded) != mp2.end() ){
+            encoded = convert();
+        }
+        
+        mp2[encoded] = longUrl;
+        return encoded;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        
+        if(mp2.find(shortUrl) != mp2.end())
+            return mp2[shortUrl];
+        return "";
+    }
+    
+    string convert(){
+        string str="";
+        for( int i=0 ; i<6 ; i++ ){
+            str+=code[rand()%67];
+        }
+        return str;
     }
 };
 
