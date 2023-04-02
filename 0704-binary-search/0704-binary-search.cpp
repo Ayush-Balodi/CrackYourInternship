251b77695bc6a8110f35ab2e3eb98bf6856bc958
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-02 19:06:59 +0530

    Time: 46 ms (21.98%), Space: 27.5 MB (93.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,16 +5,13 @@ public:
         int low=0, high=n-1;
         
         while( low<=high ){
-            int mid = (low+high)/2;
-            if( nums[mid] == target ){
+            int mid=low+(high-low)/2;
+            if( nums[mid] == target )
                 return mid;
-            }
-            else if( target < nums[mid] ){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
+            else if( nums[mid] > target )
+                high=mid-1;
+            else
+                low=mid+1;
         }
         return -1;
     }
