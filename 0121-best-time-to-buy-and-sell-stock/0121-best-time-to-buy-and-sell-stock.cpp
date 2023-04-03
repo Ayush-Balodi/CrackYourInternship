a28a0a7a7c36ee6e6705df00e5babaf68b94cd6b
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-03 19:54:46 +0530

    Time: 147 ms (28.70%), Space: 93.3 MB (91.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,9 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int min=INT_MAX, profit=0;
         for( auto x:prices ){
-            if( min > x )
-                min = x;
-            if( x-min>profit )
+            if( min>x )
+                min=x;
+            if( x-min > profit )
                 profit = x-min;
         }
         return profit;
