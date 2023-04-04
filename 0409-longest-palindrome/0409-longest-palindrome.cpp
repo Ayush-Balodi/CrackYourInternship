6135ed3e4bce0a64ff196bf8ecea7582cd38faee
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-04 15:35:20 +0530

    Time: 3 ms (70.53%), Space: 6.6 MB (87.34%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         
-        // frequency map
         unordered_map<char,int> mp;
-        for( auto x:s ) mp[x]++;
+        for( auto x:s ){
+            mp[x]++;
+        }
         
-        int result=0;
         bool odd_found=false;
+        int result=0;
         
-        //iterating over map and counting the evnen frequent characters for palindrome and further having a single count for middle element which can be odd one.
-        for( auto &x:mp ){
+        for( auto x:mp ){
             if( x.second % 2 == 0 ){
-                result += x.second;
+                result+=x.second;
             }
             else{
-                odd_found=true;
                 result += x.second-1;
+                odd_found = true;
             }
         }
-        if( odd_found ){
+        
+        if(odd_found)
             result++;
-        }
+        
         return result;
     }
 };
