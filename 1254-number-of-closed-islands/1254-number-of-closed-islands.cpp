9e6c35113f02e24994b957cf5b3da829b5721920
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-07 02:05:30 +0530

    Time: 11 ms (84.79%), Space: 9.9 MB (37.31%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,59 +1,40 @@
 class Solution {
 private:
-    void dfs( vector<vector<int>> &grid, int i , int j, int n, int m ){
-        
-        if( i<0 or i>=n or j<0 or j>=m ){
+    void dfs( vector<vector<int>> &grid, vector<vector<int>> &visited, int i, int j, int n, int m ){
+        if( i<0 || i>=n || j<0 || j>=m ){
             return;
         }
-        if(grid[i][j] != 0 ){
+        if( visited[i][j] == 1 || grid[i][j] == 1 ){
             return;
         }
-        int x[4]={0,1,-1,0};
-        int y[4]={1,0,0,-1};
-        grid[i][j] = 1;
-        for( int k=0 ; k<4 ; k++ ){
-            dfs( grid, i+x[k], j+y[k], n, m );
-        }
+        visited[i][j] = 1;
+        dfs( grid, visited, i, j+1, n, m );
+        dfs( grid, visited, i+1, j, n, m );
+        dfs( grid, visited, i, j-1, n, m );
+        dfs( grid, visited, i-1, j, n, m );
         return;
     }
 public:
     int closedIsland(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> visited(n, vector<int>(m,0));
         
-        //BORDER DFS
-        for(int i=0 ; i<n ; i++ ){
-            for(int j=0 ; j<m ; j++ ){
-                if( i*j == 0 or i==n-1 or j==m-1 ){
-                    dfs( grid, i, j, n, m );
-                }
-            }
-        }
-        
-        //REST of the regions for answer;
-        int ans=0;
         for( int i=0 ; i<n ; i++ ){
             for( int j=0 ; j<m ; j++ ){
-                if( grid[i][j] == 0 ){
-                    ans++;
-                    dfs( grid, i, j, n, m );
+                if( i*j==0 || i==n-1 || j==m-1 ){
+                    dfs( grid, visited, i, j, n, m );
                 }
             }
         }
-        return ans;
+        int count=0;
+        for( int i=0 ; i<n ; i++ ){
+            for( int j=0 ; j<m ; j++ ){
+                if( visited[i][j] == 0 && grid[i][j] == 0 ){
+                    dfs( grid, visited, i, j, n, m );
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
-/*
-     0   1   2   3   4
-  [1 1 1 1 1 1 1 0]
-  [1 0 0 0 0 1 1 0]
-  [1 0 1 0 1 1 1 0]
-  [1 0 0 0 0 1 0 1]
-  [1 1 1 1 1 1 1 0]
-    
-     0   1   2   3   4
-  0["1","1","1","1","1"]
-  1["0","0","0","1","1"]
-  2["0","0","0","1","1"]
-  3["0","0","0","1","1"]
-
-*/
