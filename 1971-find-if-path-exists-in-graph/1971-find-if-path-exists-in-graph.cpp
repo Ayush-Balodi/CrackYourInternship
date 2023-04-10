1de79880c866184ba81ae3d434b37dd6d99105cb
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-10 22:17:37 +0530

    Time: 617 ms (81.54%), Space: 155.4 MB (82.56%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> mp;
-        for( auto x:edges ){
-            vector<int> temp = x;
-            int u=temp[0];
-            int v=temp[1];
-            
-            mp[u].push_back(v);
-            mp[v].push_back(u);
-        }
-        
-        queue<int> q; vector<int> visited(n+1, 0);
-        q.push(source); visited[source] = 1;
-        
+    void BFS( vector<int> adj[], int source, vector<int> &visited ){
+        queue<int> q;
+        q.push(source); visited[source]=1;
         while( !q.empty() ){
             int curr = q.front();
             q.pop();
             
-            for( auto x:mp[curr] ){
-                if( visited[x]==0 ){
-                    visited[x]=1;
+            for( auto x:adj[curr] ){
+                if( visited[x] == 0 ){
+                    visited[x] = 1;
                     q.push(x);
                 }
             }
-            if( visited[destination] )
-                return true;
         }
+        return;
+    }
+    
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        
+        int v=edges.size();
+        vector<int> adj[n];
+        
+        if(v==0){
+            return true;
+        }
+        
+        for( int i=0 ; i<v ; i++ ){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        vector<int> visited(n+1, 0);
+        BFS(adj, source, visited);
+
         return visited[destination];
     }
 };
