5f27dd5580145bd23cc3a8ac389e8eefc9b2626a
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-13 20:19:31 +0530

    Time: 18 ms (7.65%), Space: 14 MB (83.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,34 @@
 class Solution {
-private:
-    void helper( vector<vector<int>> &image, int i, int j, int curr, int color, int n, int m){
-        if(i<0 or i>=n or j<0 or j>=m){
-            return;
-        }
-        if(image[i][j] == color){
-            return;
-        }
-        if(image[i][j] == curr){
-            image[i][j] = color;
-            helper(image, i, j+1, curr, color, n, m);
-            helper(image, i+1, j, curr, color, n, m);
-            helper(image, i-1, j, curr, color, n, m);
-            helper(image, i, j-1, curr, color, n, m);
-        }
-        return;
-    }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        int temp = image[sr][sc];
-        int n=image.size(), m=image[0].size();
-        helper(image, sr, sc, temp, color, n, m);
-        return image;
-    }
+    bool isValid(int i,int j,int n,int m,vector<vector<int>>& image,int stcolor){
+        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==stcolor) {
+            return 1;
+}
+return 0;
+}
+void dfs(int i,int j,int color,vector<vector<int>>& image,int stcolor,int n,int m){
+if(image[i][j]==color)
+    return;   
+image[i][j]=color;
+if(isValid(i-1,j,n,m,image,stcolor)){
+dfs(i-1,j,color,image,stcolor,n,m);
+}
+if(isValid(i+1,j,n,m,image,stcolor)){
+dfs(i+1,j,color,image,stcolor,n,m);
+}
+if(isValid(i,j-1,n,m,image,stcolor)){
+dfs(i,j-1,color,image,stcolor,n,m);
+}
+if(isValid(i,j+1,n,m,image,stcolor)){
+dfs(i,j+1,color,image,stcolor,n,m);
+}
+}
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+int n=image.size();
+int m=image[0].size();
+int i=sr,j=sc;
+int stcolor=image[i][j];
+dfs(i,j,color,image,stcolor,n,m);
+return image;
+}
 };
