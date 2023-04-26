346d773855cca4201922ead9ac1669fafe0d9434
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-26 23:16:10 +0530

    Time: 3 ms (56.14%), Space: 6 MB (7.98%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum=0;
+        while( num > 0 ){
+            sum += (num%10);
+            num = num/10;
+            if( sum>9 and num==0){
+                num=sum;
+                sum=0;
+            }
+        }
+        return sum;
+    }
+};
