69e7622cf6004e8b203b878ec9be3738fcd13d9e
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-04-29 00:33:36 +0530

    Time: 681 ms (62.94%), Space: 155.3 MB (82.09%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    void BFS( vector<int> adj[], int source, vector<int> &visited ){
-        queue<int> q;
-        q.push(source); visited[source]=1;
-        while( !q.empty() ){
-            int curr = q.front();
-            q.pop();
-            
-            for( auto x:adj[curr] ){
-                if( visited[x] == 0 ){
-                    visited[x] = 1;
-                    q.push(x);
-                }
-            }
-        }
-        return;
-    }
-    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         
-        int v=edges.size();
         vector<int> adj[n];
-        
-        if(v==0){
+        int v=edges.size();
+        if(v==0)
             return true;
-        }
         
-        for( int i=0 ; i<v ; i++ ){
+        for( int i=0; i<v; i++ ){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
         
+        queue<int> q;
         vector<int> visited(n+1, 0);
-        BFS(adj, source, visited);
-
+        q.push(source); visited[source]=1;
+        
+        while( !q.empty() ){
+            int curr = q.front();
+            q.pop();
+            for( auto x:adj[curr] ){
+                if( visited[x]==0 ){
+                    q.push(x);
+                    visited[x]=1;
+                }
+            }
+        }
+        
         return visited[destination];
     }
 };
