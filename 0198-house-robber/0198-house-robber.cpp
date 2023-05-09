bc8e9aaafdd2227ad5de70d2cdc4735156eccaf8
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-05-09 13:38:55 +0530

    Time: 3 ms (34.82%), Space: 7.8 MB (59.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,28 @@
 class Solution {
-public:
-    int house( int dp[] , int i , vector<int> &nums ){
-        if( i>= nums.size() ){
+private:
+    int helper(vector<int> &nums, vector<int> &dp, int i, int n){
+        if( i>=n ){
             return 0;
         }
         if( dp[i] != -1 ){
             return dp[i];
-        } 
-        int take = house( dp , i+2 , nums ) + nums[i];
-        int notake = house( dp , i+1 , nums ) + 0;
-        dp[i] = max(take,notake);
+        }
+        int take = helper(nums, dp, i+2, n) + nums[i];
+        int notake = helper(nums, dp, i+1, n) + 0;
+        dp[i] = max(take, notake);
         return dp[i];
     }
+public:
     int rob(vector<int>& nums) {
         
         int n=nums.size();
-        int dp[n+1];
-        memset( dp , -1 , sizeof(dp) );
-        return house( dp , 0 , nums);
+        vector<int> dp(n+1, -1);
+        int answer = helper(nums, dp, 0, n);
+        
+        for( int i=0 ; i<=n ; i++ ){
+            cout << dp[i] << " ";
+        }
+        
+        return answer;
     }
 };
