9a867f18a20a755722284dec342a3558cfe9a362
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-05-10 17:48:03 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (90.43%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -2,37 +2,37 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         
+        int k=0;
         vector<vector<int>> nums(n, vector<int>(n, 0));
-        int k=1;
         
         int column_start=0, column_end=n-1;
         int row_start=0, row_end=n-1;
         
-        while( column_start <= column_end and row_start <= row_end ){
+        while( column_start<=column_end and row_start<=row_end ){
             
             for( int col=column_start ; col<=column_end ; col++ ){
-                nums[row_start][col] = k++;
+                nums[row_start][col] = k+=1;
             }
             
-            row_start+=1;
+            row_start++;
             
             for( int row=row_start ; row<=row_end ; row++ ){
-                nums[row][column_end] = k++;
+                nums[row][column_end] = k+=1;
             }
             
-            column_end-=1;
+            column_end--;
             
             for( int col=column_end ; col>=column_start ; col-- ){
-                nums[row_end][col] = k++;
+                nums[row_end][col] = k+=1;
             }
             
-            row_end-=1;
+            row_end--;
             
             for( int row=row_end ; row>=row_start ; row-- ){
-                nums[row][column_start] = k++;
+                nums[row][column_start] = k+=1;
             }
             
-            column_start+=1;
+            column_start++;
         }
         return nums;
     }
