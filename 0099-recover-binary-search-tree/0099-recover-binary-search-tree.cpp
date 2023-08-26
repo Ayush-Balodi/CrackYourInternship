3b1c15adda4afea118ec4f19a8cd10f1cb008c06
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-08-27 01:21:52 +0530

    Time: 28 ms (65.85%), Space: 57.9 MB (33.84%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -9,31 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*This is the solution with time complexicity O(n) and space as O(1) as I haven't considered the stack as space. In previous solution I have utilized the preorder sequence and sorted it to construct a correct BST but now I will mark correction in the same by updating the unsorted members
+There will be two cases:
+i> they are adjacent
+ii> they are not adjacent
+*/
 class Solution {
+private:
+    TreeNode *prev, *middle, *last, *first;
 public:
-    vector<int> v;
-    int i=0;
-    void helper( TreeNode* curr ){
-        if( curr == NULL ){
+    void inorder( TreeNode* root ){
+        if( root == NULL ){
             return;
         }
-        helper( curr->left );
-        v.push_back(curr->val);
-        helper( curr->right );
+        inorder( root->left );
+        if( (prev!= NULL) and (root->val < prev->val) ){
+            
+            //first violation mark first and middle
+            if( first == NULL ){
+                first = prev;
+                middle = root;
+            }
+            // this is for the second time of violation
+            else{
+                last = root;
+            }
+        }
+        //marking the previous
+        prev = root;
+        inorder( root->right );
         return;
     }
-    void helper2( TreeNode* root ){
-        if(root == NULL){
-            return;
-        }
-        helper2(root->left);
-        root->val = v[i++];
-        helper2(root->right);
-    }
+    
     void recoverTree(TreeNode* root) {
-        helper(root);
-        sort(v.begin(), v.end());
-        helper2(root);
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if( first && last ){ swap(first->val, last->val); }
+        else if( first && middle ){ swap(first->val, middle->val); }
         return;
     }
 };
