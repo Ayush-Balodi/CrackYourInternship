5cc9c2102542bc0a2464a58a01d09c95eb788ae2
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-09-10 00:08:35 +0530

    Time: 38 ms (23.19%), Space: 10.7 MB (57.32%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -3,32 +3,34 @@ public:
     string removeDuplicates(string s, int k) {
         
         stack<pair<char,int>> st;
+        string result = "";
         int n=s.size();
+        
         for( int i=0 ; i<n ; i++ ){
             
             if( st.empty() or s[i] != st.top().first ){
-                st.push({s[i], 1});
+                st.push({s[i],1});
             }
             else if( s[i] == st.top().first ){
-                auto prev = st.top();
+                auto prev=st.top();
                 st.pop();
-                st.push({prev.first, prev.second+1});
+                st.push({prev.first,prev.second+1});
             }
             if( st.top().second == k ){
                 st.pop();
             }
         }
-        string str="";
+        
         while( !st.empty() ){
             auto curr = st.top();
             st.pop();
-            while( curr.second-- ){
-                str += curr.first;
+            while( curr.second --  ){
+                result += curr.first;
             }
         }
         
-        reverse(str.begin(), str.end());
+        reverse( result.begin(), result.end() );
         
-        return str;
+        return result;
     }
 };
