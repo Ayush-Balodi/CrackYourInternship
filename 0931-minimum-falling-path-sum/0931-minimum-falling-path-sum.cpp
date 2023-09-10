eb5b09b4b9ab60399c7afc52dd8ff4b5200aeb90
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-09-10 17:20:35 +0530

    Time: 8 ms (94.65%), Space: 10.7 MB (50.74%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n=matrix.size(), m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        
-        for( int i=0 ; i<n; i++ ){
-            for( int j=0 ; j<m ; j++ ){
-                if( i==0 ){
+        int n=matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        for( int i=0 ; i<n ; i++ ){
+            for( int j=0 ; j<n ; j++ ){
+                
+                if( i == 0 ){
                     dp[i][j] = matrix[i][j];
                     continue;
                 }
-                if( j==0 ){
-                    dp[i][j] = min((dp[i-1][j] + matrix[i][j]) , (dp[i-1][j+1] + matrix[i][j]));
+                if( j == 0 ){
+                    dp[i][j] = min((dp[i-1][j]+matrix[i][j]), (dp[i-1][j+1]+matrix[i][j]));
                 }
-                else if( j==m-1 ){
-                    dp[i][j] = min((dp[i-1][j] + matrix[i][j]),(dp[i-1][j-1] + matrix[i][j]));
+                else if( j == n-1 ){
+                    dp[i][j] = min((dp[i-1][j]+matrix[i][j]), (dp[i-1][j-1]+matrix[i][j]));
                 }
                 else{
-                    dp[i][j] = min(min((dp[i-1][j] + matrix[i][j]),(dp[i-1][j+1] + matrix[i][j])),(dp[i-1][j-1] + matrix[i][j]));
+                    dp[i][j] = min(min(dp[i-1][j]+matrix[i][j], dp[i-1][j-1]+matrix[i][j]), dp[i-1][j+1]+matrix[i][j]);
                 }
             }
         }
-        int minval = INT_MAX;
-        for( int i=0 ; i<m ; i++ ){
-            minval = min(minval,dp[n-1][i]);
-        }
-        return minval;
+        
+        return *min_element(begin(dp[n-1]), end(dp[n-1]));
     }
 };
