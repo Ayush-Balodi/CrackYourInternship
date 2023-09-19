56ced8938e369f249d826e823a991b6c78bb55d4
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-09-19 15:49:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (14.26%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,13 +3,11 @@ public:
     string mergeAlternately(string word1, string word2) {
         
         int m=word1.size(), n=word2.size();
-        int i=0, j=0;
         string result="";
         
-        while( i<m or j<n ){
-            
-            if( i<m ){ result.push_back(word1[i++]); }
-            if( j<n ){ result.push_back(word2[j++]); }
+        for( int i=0 ; i<max(m, n) ; i++ ){    
+            if( i<m ){ result.push_back(word1[i]); }
+            if( i<n ){ result.push_back(word2[i]); }
         }
         return result;
     }
