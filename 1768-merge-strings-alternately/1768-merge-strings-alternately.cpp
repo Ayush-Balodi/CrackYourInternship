21af0d7aaf9c95e4405640dfd302ae5fd004ff1c
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-09-19 15:44:46 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (11.91%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int n=word1.length(), m=word2.length();
-        int i=0, j=0, flag=1;
-        string ans="";
-        while( i<n && j<m ){
-            if( flag ){
-                ans += word1[i++];
-                flag=0;
-            }
-            if( !flag ){
-                ans += word2[j++];
-                flag=1;
-            }
-        }
         
-        while( i<n ) ans += word1[i++];
-        while( j<m ) ans += word2[j++];
-        return ans;
+        int m=word1.size(), n=word2.size();
+        int i=0, j=0;
+        string result="";
+        
+        while( i<m or j<n ){
+            
+            if( i<m ){ result.push_back(word1[i++]); }
+            if( j<n ){ result.push_back(word2[j++]); }
+        }
+        return result;
     }
 };
