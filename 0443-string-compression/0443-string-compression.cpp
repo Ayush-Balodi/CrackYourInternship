2c01c631b173f1ba6bcc13f3655c942f536d2b24
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-09-21 09:18:56 +0530

    Time: 3 ms (82.90%), Space: 9.4 MB (6.57%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -2,45 +2,38 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         
-        vector<char> ch;
         int n=chars.size(), count=1;
-        ch.push_back( chars[0] );
+        vector<char> ch;
         for( int i=1 ; i<n ; i++ ){
-            if( chars[i-1] == chars[i] ){
-                count+=1;
-            }
-            else{
-                if( count!=1 ){
-                    if( count > 9 ){
-                        string temp = to_string(count);
-                        for( auto x:temp ){
-                            ch.push_back(x);
-                        }
-                    }
-                    else{
-                        ch.push_back(count+'0');
-                    }
-                }
-                ch.push_back(chars[i]);
-                count=1;
-            }
-        }
-        if( count != 1 ){
-            if( count > 9 ){
-                string temp = to_string(count);
+            
+            if(chars[i] != chars[i-1]){
+                if( count==1 ){ ch.push_back(chars[i-1]); continue; }
+                ch.push_back(chars[i-1]);
+                string temp=to_string(count);
                 for( auto x:temp ){
                     ch.push_back(x);
                 }
+                count=1;
             }
             else{
-                ch.push_back(count+'0');
+                count++;
             }
         }
-        
-        chars.resize(ch.size());
-        for( int i=0 ; i<ch.size(); i++ ){
+        if(count!=1){
+            ch.push_back(chars[n-1]);
+            string temp=to_string(count);
+            for( auto x:temp ){
+                ch.push_back(x);
+            }
+        }
+        else{
+            ch.push_back(chars[n-1]);
+        }
+        n=ch.size();
+        chars.resize(n);
+        for( int i=0 ; i<n ; i++ ){
             chars[i] = ch[i];
         }
-        return ch.size();
+        return n;
     }
 };
